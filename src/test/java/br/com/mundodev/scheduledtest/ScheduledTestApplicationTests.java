573f2fc9a32a867c464e50261af4c497c5e4e26d
Iago Reis <ireis@uolinc.com>
2020-03-10 08:58:51 -0300

    Ah shit, here we go again

---
+++ src/test/java/br/com/mundodev/scheduledtest/ScheduledTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mundodev.scheduledtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
